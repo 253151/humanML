53c7bfc0cbe03b32958c4028aef3a338c637ca89
253151 <wdyl1923@gmail.com>
2023-01-09 17:14:51 +0900

    updated

---
+++ sel-dongju.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import urllib.request as req
+
+url = "https://ko.wikisource.org/wiki/%ED%95%98%EB%8A%98%EA%B3%BC_%EB%B0%94%EB%9E%8C%EA%B3%BC_%EB%B3%84%EA%B3%BC_%EC%8B%9C"
+res = req.urlopen(url)
+soup = BeautifulSoup(res, "html.parser")
+
+a_list = soup.select("#mw-content-text > div > ul > li a")
+
+
+for a in a_list:
+    name = a.string
+    print("-", name)
+
+
+
+    #mw-content-text > div.mw-parser-output > ul:nth-child(6) > li > b > a
